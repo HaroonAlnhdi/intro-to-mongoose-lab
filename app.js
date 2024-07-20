4622b75921374de2bceb9d694c6f15ca6941be1c
Haroon <you@example.com>
2024-07-20 16:48:11 +0300

    fixing error

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-const prompt = require('prompt-sync')();
-const dotenv = require('dotenv');
-dotenv.config();
-const mongoose = require('mongoose');
-const Customer = require('./models/customer');
+                const prompt = require('prompt-sync')();
+                const dotenv = require('dotenv');
+                dotenv.config();
+                const mongoose = require('mongoose');
+                const Customer = require('./models/customer');
 
-// const username = prompt('What is your name? ');
+                // const username = prompt('What is your name? ');
 
-// console.log(`Your name is ${username}`);
-
-
-// connection to MongoDB
-const connect = async () => {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('Connected to MongoDB');
-    await runQueries()
-    await mongoose.disconnect();
-    console.log('Disconnected from MongoDB');
-    process.exit();
-};
+                // console.log(`Your name is ${username}`);
 
 
-const runQueries = async () => {
-    console.log('Queries running.')
-    //    await displayMessage();
-       await run();
-    
-  }; 
+                // connection to MongoDB
+                const connect = async () => {
+                    await mongoose.connect(process.env.MONGODB_URI);
+                    console.log('Connected to MongoDB');
+                    await runQueries()
+                    await mongoose.disconnect();
+                    console.log('Disconnected from MongoDB');
+                    process.exit();
+                };
 
-  connect()
+
+                const runQueries = async () => {
+                    console.log('Queries running.')
+                    //    await displayMessage();
+                    await app();
+                    
+                }; 
+
+                connect()
   /*-------------------------------- Query Functions --------------*/
 
                 const displayMessage = async () => {
@@ -74,7 +74,7 @@ const runQueries = async () => {
                     console.log('Removed customer:', removedCustomer);
                 };
 
-                const run = async () => {
+                const app = async () => {
                     let action;
                   
                         action = await displayMessage();
